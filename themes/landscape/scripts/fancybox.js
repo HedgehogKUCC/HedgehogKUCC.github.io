95a3fdb80faacdeebede1c683b35b6842c18994a
HedgehogKU <ccc908925@gmail.com>
2020-02-25 10:46:05 +0800

    DELETE span.caption

--- themes/landscape/scripts/fancybox.js
+++ themes/landscape/scripts/fancybox.js
@@ -17,8 +17,11 @@ hexo.extend.tag.register('fancybox', function(args){
 
   var title = args.join(' ');
 
+  // return '<a class="fancybox" href="' + original + '" title="' + title + '">' +
+  //   '<img src="' + (thumbnail || original) + '" alt="' + title + '">'
+  //   '</a>' +
+  //   (title ? '<span class="caption">' + title + '</span>' : '');
   return '<a class="fancybox" href="' + original + '" title="' + title + '">' +
     '<img src="' + (thumbnail || original) + '" alt="' + title + '">'
-    '</a>' +
-    (title ? '<span class="caption">' + title + '</span>' : '');
+    '</a>';
 });
